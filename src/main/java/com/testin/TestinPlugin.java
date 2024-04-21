fbb4baeba3c4ed5724822e08bb63403bf83a3bbd
Justin <justinblackemail@gmail.com>
2024-04-21 17:16:30 -0500

    testin

--- src/main/java/com/testin/TestinPlugin.java
+++ src/main/java/com/testin/TestinPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.testin;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class TestinPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TestinConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    TestinConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TestinConfig.class);
 	}
 }
